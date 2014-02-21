0bd9225b17e248a347fad3cc04e1240b18947c7a
yorn <git@yorn.priv.no>
2014-02-21 17:57:16 +0100

    End sentences with full stop
    as recommended on
    http://www.mediawiki.org/wiki/Localisation#Use_full_stops

--- SimpleSamlAuth.i18n.php
+++ SimpleSamlAuth.i18n.php
@@ -16,48 +16,48 @@ $messages = array();
  * @author Kghbln
  */
 $messages['en'] = array(
-        'simplesamlauth-desc' => 'Enables SAML authentication using [https://simplesamlphp.org/ SimpleSAMLphp]'
+	'simplesamlauth-desc' => 'Enables SAML authentication using [https://simplesamlphp.org/ SimpleSAMLphp].',
 );
 
 /** German (Deutsch)
  * @author Kghbln
  */
 $messages['de'] = array(
-        'simplesamlauth-desc' => 'Ermöglicht SAML-Authentifizierung mit [https://simplesamlphp.org/ SimpleSAMLphp]'
+	'simplesamlauth-desc' => 'Ermöglicht SAML-Authentifizierung mit [https://simplesamlphp.org/ SimpleSAMLphp].',
 );
 
 /** Norwegian (bokmål)
  * @author Yørn de Jong
  */
 $messages['nb'] = array(
-        'simplesamlauth-desc' => 'Gjør SAML-autentisering mulig ved hjelp av [https://simplesamlphp.org/ SimpleSAMLphp]'
+	'simplesamlauth-desc' => 'Gjør SAML-autentisering mulig ved hjelp av [https://simplesamlphp.org/ SimpleSAMLphp].',
 );
 
 /** Norwegian (nynorsk)
  * @author Yørn de Jong
  */
 $messages['nn'] = array(
-        'simplesamlauth-desc' => 'Gjer SAML-autentisering mogeleg ved hjelp av [https://simplesamlphp.org/ SimpleSAMLphp]'
+	'simplesamlauth-desc' => 'Gjer SAML-autentisering mogeleg ved hjelp av [https://simplesamlphp.org/ SimpleSAMLphp].',
 );
 
 /** Dutch (Nederlands)
  * @author Yørn de Jong
  */
 $messages['nl'] = array(
-        'simplesamlauth-desc' => 'Maakt SAML-authenticatie mogelijk via [https://simplesamlphp.org/ SimpleSAMLphp]'
+	'simplesamlauth-desc' => 'Maakt SAML-authenticatie mogelijk via [https://simplesamlphp.org/ SimpleSAMLphp].',
 );
 
 /** Frisian (Frysk)
  * @author Yørn de Jong
  */
 $messages['fy'] = array(
-        'simplesamlauth-desc' => 'Makket SAML-autentikaasje mooglik fia [https://simplesamlphp.org/ SimpleSAMLphp]'
+	'simplesamlauth-desc' => 'Makket SAML-autentikaasje mooglik fia [https://simplesamlphp.org/ SimpleSAMLphp].',
 );
 
 /** Estonian                                                                    
  * @author Kristian Kankainen                                                   
  */
 $messages['et'] = array(
-        'simplesamlauth-desc' => 'Võimaldab SAML-iga autentimise kasutades [https://simplesamlphp.org/ SimpleSAMLphp]'
+	'simplesamlauth-desc' => 'Võimaldab SAML-iga autentimise kasutades [https://simplesamlphp.org/ SimpleSAMLphp].',
 );
 
