21cb5475aca79752fcf167db70c1633ce67fdf87
yorn <git@yorn.priv.no>
2014-02-25 23:55:39 +0100

    Don't link to secure site when Wiki is not secure

--- SimpleSamlAuth.i18n.php
+++ SimpleSamlAuth.i18n.php
@@ -16,7 +16,7 @@ $messages = array();
  * @author Kghbln
  */
 $messages['en'] = array(
-	'simplesamlauth-desc' => 'Enables SAML authentication using [https://simplesamlphp.org/ SimpleSAMLphp].',
+	'simplesamlauth-desc' => 'Enables SAML authentication using [//simplesamlphp.org/ SimpleSAMLphp].',
 	'simplesamlauth-login' => 'Log in using SAML',
 	'simplesamlauth-pagedisabled' => 'This page is disabled by the [//github.com/yorn/mwSimpleSamlAuth SimpleSamlAuth] extension.',
 );
@@ -25,14 +25,14 @@ $messages['en'] = array(
  * @author Kghbln
  */
 $messages['de'] = array(
-	'simplesamlauth-desc' => 'Ermöglicht SAML-Authentifizierung mit [https://simplesamlphp.org/ SimpleSAMLphp].',
+	'simplesamlauth-desc' => 'Ermöglicht SAML-Authentifizierung mit [//simplesamlphp.org/ SimpleSAMLphp].',
 );
 
 /** Norwegian (bokmål)
  * @author Yørn de Jong
  */
 $messages['nb'] = array(
-	'simplesamlauth-desc' => 'Gjør SAML-autentisering mulig ved hjelp av [https://simplesamlphp.org/ SimpleSAMLphp].',
+	'simplesamlauth-desc' => 'Gjør SAML-autentisering mulig ved hjelp av [//simplesamlphp.org/ SimpleSAMLphp].',
 	'simplesamlauth-login' => 'Logg inn ved hjelp av SAML',
 );
 
@@ -40,7 +40,7 @@ $messages['nb'] = array(
  * @author Yørn de Jong
  */
 $messages['nn'] = array(
-	'simplesamlauth-desc' => 'Gjer SAML-autentisering mogeleg ved hjelp av [https://simplesamlphp.org/ SimpleSAMLphp].',
+	'simplesamlauth-desc' => 'Gjer SAML-autentisering mogeleg ved hjelp av [//simplesamlphp.org/ SimpleSAMLphp].',
 	'simplesamlauth-login' => 'Logg inn ved hjelp av SAML',
 );
 
@@ -48,7 +48,7 @@ $messages['nn'] = array(
  * @author Yørn de Jong
  */
 $messages['nl'] = array(
-	'simplesamlauth-desc' => 'Maakt SAML-authenticatie mogelijk via [https://simplesamlphp.org/ SimpleSAMLphp].',
+	'simplesamlauth-desc' => 'Maakt SAML-authenticatie mogelijk via [//simplesamlphp.org/ SimpleSAMLphp].',
 	'simplesamlauth-login' => 'Aanmelden via SAML',
 );
 
@@ -56,7 +56,7 @@ $messages['nl'] = array(
  * @author Yørn de Jong
  */
 $messages['fy'] = array(
-	'simplesamlauth-desc' => 'Makket SAML-autentikaasje mooglik fia [https://simplesamlphp.org/ SimpleSAMLphp].',
+	'simplesamlauth-desc' => 'Makket SAML-autentikaasje mooglik fia [//simplesamlphp.org/ SimpleSAMLphp].',
 	'simplesamlauth-login' => 'Oanmelde mei SAML',
 );
 
@@ -64,6 +64,6 @@ $messages['fy'] = array(
  * @author Kristian Kankainen                                                   
  */
 $messages['et'] = array(
-	'simplesamlauth-desc' => 'Võimaldab SAML-iga autentimise kasutades [https://simplesamlphp.org/ SimpleSAMLphp].',
+	'simplesamlauth-desc' => 'Võimaldab SAML-iga autentimise kasutades [//simplesamlphp.org/ SimpleSAMLphp].',
 );
 
