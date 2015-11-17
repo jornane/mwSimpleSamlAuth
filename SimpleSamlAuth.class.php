jornane/mwSimpleSamlAuth

commit 587e384fb63155a10aa94f332ebbdb73e41d8192

Overwrite `$wgSessionName` if appropriate.
Fixes #21

SimpleSamlAuth.class.php
@@ -52,6 +52,14 @@ class SimpleSamlAuth {
 			return true;
 		}
 
+		if((!isset($wgSessionName) || !$wgSessionName)
+			&& (!isset($wgSessionsInObjectCache) || !$wgSessionsInObjectCache)
+			&& (!isset($wgSessionsInMemcached) || !$wgSessionsInMemcached)
+		) {
+			$wgSessionName = ini_get('session.name');
+		}
+
+
 		// Load the simpleSamlPhp service
 		require_once rtrim( $wgSamlSspRoot, DIRECTORY_SEPARATOR ) .
 			DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . '_autoload.php';

commit 1dc8087949f4565f0e2f4f26fdb8aded279aa202

Fix regression introduced in 587e384
Global variables must be declared as such.

SimpleSamlAuth.class.php
@@ -44,6 +44,9 @@ class SimpleSamlAuth {
 	private static function init() {
 		global $wgSamlSspRoot;
 		global $wgSamlAuthSource;
+		global $wgSessionName;
+		global $wgSessionsInMemcached;
+		global $wgSessionsInObjectCache;
 
 		if ( !self::$armed ) {
 			return false;